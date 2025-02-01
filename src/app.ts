b3adc92a97cc1fa59bd0e7ff0d40f9cb2a8368c7
Marc Wolpert <mewolpert@gmail.com>
2025-01-31 17:19:31 -0800

    feat: Finished message board

--- src/app.ts
+++ src/app.ts
@@ -9,16 +9,33 @@ app.set('view engine', 'ejs');
 
 const assetsPath: string = path.join(process.cwd(), 'src', 'public');
 app.use(express.static(assetsPath));
+app.use(express.urlencoded({ extended: true }));
 
-const links: { href: String; text: string }[] = [
-	{ href: '/', text: 'Home' },
-	{ href: 'about', text: 'About' },
+const messages: { text: string; user: string; added: Date }[] = [
+	{ text: 'Hi there!', user: 'Amando', added: new Date() },
+	{
+		text: 'Hello World!',
+		user: 'Charles',
+		added: new Date(),
+	},
 ];
-const users = ['Rose', 'Cake', 'Biff'];
 
 //passing in objects to render in ejs
 app.get('/', (req: Request, res: Response) => {
-	res.render('index', { message: 'EJS Rocks!', links, users });
+	res.render('index', { title: 'Mini Message Board', messages: messages });
+});
+app.get('/new', (req: Request, res: Response) => {
+	res.render('form', { title: 'Message Form' });
+});
+app.post('/new', (req: Request, res: Response) => {
+	//access the POST request's data with req.body
+	messages.push({
+		text: req.body.message_text,
+		user: req.body.message_user,
+		added: new Date(),
+	});
+	// res.render('new', { title: 'Mini Message Board', messages: messages });
+	res.redirect('/');
 });
 app.all('*', (req: Request, res: Response) => {
 	res.send('404');
